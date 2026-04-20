225d43ce5c46969903a924882729bf868ee32b0e
Syauqi Auliya (Cho) <syauqido>
2026-04-20 10:43:14 +0800

    Initial Commit

---
+++ CH2RemixCreate/CH2RemixCreateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH2RemixCreateApp.swift
+//  CH2RemixCreate
+//
+//  Created by Syauqi Auliya M on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CH2RemixCreateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH2RemixCreate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH2RemixCreate
+//
+//  Created by Syauqi Auliya M on 20/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
