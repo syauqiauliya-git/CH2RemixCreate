312a1da9dba25f31aaf8b718cfb61c2a037909a7
Gleen Ryan <tjiagleenryan@gmail.com>
2026-04-24 10:26:12 +0800

    lesson done

--- CH2RemixCreate/Screens/LessonScreen.swift
+++ CH2RemixCreate/Screens/LessonScreen.swift
@@ -5,3 +5,111 @@
 //  Created by Gleenryan on 23/04/26.
 //
 
+import SwiftUI
+
+
+struct LessonScreen: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    let currentLesson:Lesson
+    
+    var body: some View {
+        
+        
+        
+        //        let currentQuestion = quiz.questions[currentQuestionIndex]
+        
+        VStack {
+//            Spacer()
+            Spacer()
+            ZStack(){
+                Circle()
+                    .fill(Color(hex: "C9F55F"))
+                    .frame(width: 100)
+                    .opacity(0.9)
+                    
+                    
+                if let image = currentLesson.image{
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                }
+            }
+            
+            Spacer()
+            Text(currentLesson.title)
+                .font(.system(size: 28, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 32)
+       
+            
+           
+            
+     
+            
+            //card
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white.opacity(0.4))
+                  
+                    .stroke(Color.white).opacity(0.3)
+                    
+                
+                
+                VStack(spacing:10){
+                    
+
+                    
+                Text(currentLesson.description)
+                    .font(.system(size: 19, weight: .regular, design: .rounded))
+                    .multilineTextAlignment(.leading)
+                    .lineSpacing(7)
+                    .padding(.horizontal)
+                }
+            }
+            .frame(height: 200)
+            .padding(20)
+            
+            Spacer()
+            
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                HStack{
+                    Text("Next")
+                        .bold()
+                    Image(systemName: "arrow.right")
+                        .font(.system(size: 15, weight: .bold))
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color(hex: "C9F55F").opacity(0.75))
+                .foregroundColor(.white)
+                .cornerRadius(20)
+                .glassEffect()
+            }
+            .padding(.bottom, 10)
+            .padding(.horizontal)
+            
+            
+
+            
+        }
+        .background(Color(hex: "393B55"))
+        
+    }
+}
+        
+#Preview {
+    let lesson = Lesson(
+        title: "What is a Stock?",
+        description: "A stock is like owning a tiny piece of a company! If the company does well, your tiny piece becomes more valuable. It’s like sharing a pizza with friends, but instead of pizza, you share a company!",
+        image: Image(systemName: "building.2.fill")
+    )
+    LessonScreen(currentLesson: lesson)
+}
